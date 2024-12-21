033f2d808c566189bc122720458b6f92bfbafd25
JRLee <49389212+JReal2@users.noreply.github.com>
2024-12-21 19:15:57 +0900

    first commit

---
+++ fcboard/src/main/java/com/fastcampus/fcboard/FcboardApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.fcboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  FcboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcboardApplication.class, args);
+    }
+
+}
