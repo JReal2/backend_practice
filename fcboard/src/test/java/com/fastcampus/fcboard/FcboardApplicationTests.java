c5956e1005e2c052550be8a024d5690785b430c8
JReal2 <dlwjdfuf99@gmail.com>
2025-01-04 13:32:44 +0900

    도메인 jpa 작동 테스트

--- fcboard/src/test/java/com/fastcampus/fcboard/FcboardApplicationTests.java
+++ fcboard/src/test/java/com/fastcampus/fcboard/FcboardApplicationTests.java
@@ -1,7 +1,11 @@
 package com.fastcampus.fcboard;
 
+import com.fastcampus.fcboard.domain.Article;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class FcboardApplicationTests {
